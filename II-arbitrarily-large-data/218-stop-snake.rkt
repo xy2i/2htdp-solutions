b05b33671991cbe6b353f1c0b1bf3b0ded55c8ff
xy2 <xy2@cock.li>
2019-08-08 07:01:13 +0200

    218

--- II-arbitrarily-large-data/218-stop-snake.rkt
+++ II-arbitrarily-large-data/218-stop-snake.rkt
@@ -12,10 +12,18 @@
 
 ; A Snake is a List-of-segments.
 
+(define-struct sgame [snake food])
+; A Snake-game is a struct.
+; (make-sg Snake Posn)
+; creates a game with a snake (a List-of-segments)
+; and a single food at the posn
+
 (define SNAKE (circle 5 "solid" "red"))
 (define SNAKE-HEAD (circle 5 "solid" "orange"))
+(define FRUIT (overlay/xy (rectangle 4 7 "solid" "red") -3 2 (circle 5 "solid" "green")))
 
 (define LSIZE 10) ; the logical size of the scene
+(define MAX LSIZE) 
 (define OFFSET (/ (image-height SNAKE) 2)) ; offset to place the snake with place-image
 (define BG (empty-scene (* (image-height SNAKE) LSIZE)
                         (* (image-height SNAKE) LSIZE))) ; logical size of the scene: 10 * 10
@@ -25,21 +33,35 @@
 (define LEFT (make-posn -1 0))
 (define RIGHT (make-posn 1 0))
 
+; Snake-game -> Snake-game
+; moves the snake, checks for eating food and creates new food if eaten
+(define (tock sg)
+  (if (eaten-food? sg)
+      (grow-snake-change-food sg)
+      (make-sgame
+       (move/snake (sgame-snake sg))
+       (sgame-food sg))))
+
 ; Snake -> Snake
 ; moves an entire snake
-(check-expect (move (list
+(check-expect (move/snake (list
                      (make-seg (make-posn 0 0) "right")
                      (make-seg (make-posn 1 0) "right")))
               (list (make-seg (make-posn 1 0) "right")
                     (make-seg (make-posn 2 0) "right")))
-(define (move s)
+(check-expect (move/snake (list
+                     (make-seg (make-posn 0 0) "right")
+                     (make-seg (make-posn 1 0) "down")))
+              (list (make-seg (make-posn 1 0) "down")
+                    (make-seg (make-posn 1 1) "down")))
+(define (move/snake s)
   (cond
     [(empty? (rest s)) ; only one segment?
      (cons (move/segment (first s) (seg-dir (first s))) ; move it in its direction
            '())]
     [else
      (cons (move/segment (first s) (seg-dir (second s))) ; at least two segments
-           (move (rest s)))]))
+           (move/snake (rest s)))]))
     
 ; Segment -> Segment
 ; moves a segment one logical unit per tick,
@@ -64,17 +86,70 @@
     [(string=? "up" str) UP]
     [(string=? "down" str) DOWN]))
 
+; Snake-game -> Boolean
+; check if the food was eaten
+(check-expect (eaten-food?
+               (make-sgame
+                (list (make-posn 0 0)
+                      (make-posn 0 1))
+                (make-posn 2 2)) #false))
+(check-expect (eaten-food?
+               (make-sgame
+                (list (make-posn 0 0)
+                      (make-posn 0 1))
+                (make-posn 0 1)) #false))
+(define (eaten-food? sg)
+  (member? (sgame-food sg) ; posn of food
+           (segs-posns (sgame-snake sg)))) ; list of posns for the snake
+
+; Posn -> Posn 
+; generates a random food, if the food is not in the same place
+(check-satisfied (food-create (make-posn 1 1)) not=-1-1?)
+(define (food-create p)
+  (food-check-create
+     p (make-posn (random MAX) (random MAX))))
+ 
+; Posn Posn -> Posn 
+; generative recursion 
+; checks if the food is not in the same place, calls food-create again otherwise
+(define (food-check-create p candidate)
+  (if (equal? p candidate) (food-create p) candidate))
+ 
+; Posn -> Boolean
+; use for testing only 
+(define (not=-1-1? p)
+  (not (and (= (posn-x p) 1) (= (posn-y p) 1))))
+
+; Snake-game -> Snake-game
+; turns the snake in a snake-game
+(check-expect (turn
+               (make-sgame (list (make-seg (make-posn 2 0) "left")
+                                 (make-seg (make-posn 1 0) "left")
+                                 (make-seg (make-posn 0 0) "down")
+                                 (make-seg (make-posn 0 1) "down"))
+                           (make-posn 5 5))
+               "right")
+              (make-sgame (list (make-seg (make-posn 2 0) "left")
+                                 (make-seg (make-posn 1 0) "left")
+                                 (make-seg (make-posn 0 0) "down")
+                                 (make-seg (make-posn 0 1) "right"))
+                          (make-posn 5 5)))
+(define (turn sg ke)
+  (make-sgame
+   (turn/snake (sgame-snake sg) ke)
+   (sgame-food sg)))
+  
 ; Snake -> Snake
 ; turns the snake, without changing its position
-(check-expect (turn (list (make-seg (make-posn 2 0) "left")
-                          (make-seg (make-posn 1 0) "left")
-                          (make-seg (make-posn 0 0) "down")
-                          (make-seg (make-posn 0 1) "down")) "right")
+(check-expect (turn/snake (list (make-seg (make-posn 2 0) "left")
+                                (make-seg (make-posn 1 0) "left")
+                                (make-seg (make-posn 0 0) "down")
+                                (make-seg (make-posn 0 1) "down")) "right")
               (list (make-seg (make-posn 2 0) "left")
                     (make-seg (make-posn 1 0) "left")
                     (make-seg (make-posn 0 0) "down")
                     (make-seg (make-posn 0 1) "right")))
-(define (turn s ke)
+(define (turn/snake s ke)
   (cond
     [(empty? (rest s)) ; only turn the first segment
      (cons
@@ -86,19 +161,31 @@
                   [(string=? "down" ke) "down"]
                   [else (seg-dir (first s))]))
       '())]
-    [else (cons (first s) (turn (rest s) ke))]))
+    [else (cons (first s) (turn/snake (rest s) ke))]))
 
-; Segment -> Image
-; renders a single segment
-(check-expect (render (list (make-seg (make-posn 0 0) "left")))
+; Snake-game -> Image
+; renders a snake, and food
+(check-expect (render (make-sgame (list (make-seg (make-posn 0 0) "left"))
+                                  (make-posn 2 2)))
+                      (place-image FRUIT 25 25 (render/snake
+                                                (list (make-seg (make-posn 0 0) "left")))))
+(define (render sg)
+  (place-image FRUIT
+               (+ OFFSET (* (posn-x (sgame-food sg)) 10))
+               (+ OFFSET (* (posn-y (sgame-food sg)) 10))
+               (render/snake (sgame-snake sg))))
+
+; Snake -> Image
+; renders a snake
+(check-expect (render/snake (list (make-seg (make-posn 0 0) "left")))
               (place-image SNAKE-HEAD 5 5 BG))
-(check-expect (render (list (make-seg (make-posn 2 3) "left")))
+(check-expect (render/snake (list (make-seg (make-posn 2 3) "left")))
               (place-image SNAKE-HEAD 25 35 BG))
-(check-expect (render (list (make-seg (make-posn 0 0) "left")
+(check-expect (render/snake (list (make-seg (make-posn 0 0) "left")
                             (make-seg (make-posn 1 0) "left")))
               (place-image SNAKE-HEAD 15 5 (place-image SNAKE 5 5 BG)))
 
-(define (render s)
+(define (render/snake s)
   (cond
     [(empty? (rest s))
      (place-image SNAKE-HEAD
@@ -109,29 +196,33 @@
      (place-image SNAKE
                   (+ OFFSET (* (posn-x (seg-posn (first s))) 10))
                   (+ OFFSET (* (posn-y (seg-posn (first s))) 10))
-                  (render (rest s)))]))
+                  (render/snake (rest s)))]))
 
-; Snake -> Image
+; Snake-game -> Image
 ; renders the snake, and says he's out
 (define (render/out s)
   (overlay (text
             (cond
-              [(oob? s) "worm hit wall"]
-              [(self-eat? s) "worm ate himself"])
+              [(oob? (sgame-snake s)) "worm hit wall"]
+              [(self-eat? (sgame-snake s)) "worm ate himself"])
             12
             "black")
            (render s)))
   
-; Snake -> Boolean
+; Snake-game -> Boolean
 ; check if the snake is out of bounds
-(check-expect (out? (list (make-seg (make-posn 0 0) "left")))
+(check-expect (out? (make-sgame (list (make-seg (make-posn 0 0) "left"))
+                                (make-posn 5 5)))
               #false)
-(check-expect (out? (list (make-seg (make-posn -1 0) "left")))
+(check-expect (out? (make-sgame (list (make-seg (make-posn -1 0) "left"))
+                                (make-posn 5 5)))
               #true)
-(check-expect (out? (list (make-seg (make-posn 10 10) "left")))
+(check-expect (out? (make-sgame (list (make-seg (make-posn 10 10) "left"))
+                                (make-posn 5 5)))
               #true)
-(define (out? s)
-  (or (oob? s) (self-eat? s)))
+(define (out? sg)
+  (or (oob? (sgame-snake sg))
+      (self-eat? (sgame-snake sg))))
 
 ; Snake -> Boolean
 ; check if a snake is out of bounds (if its head is out)
@@ -141,6 +232,7 @@
    (< (posn-y (seg-posn (last s))) 0)
    (> (posn-x (seg-posn (last s))) (sub1 LSIZE))
    (> (posn-y (seg-posn (last s))) (sub1 LSIZE))))
+
 ; Snake -> Segment
 ; get the last segment
 (define (last s)
@@ -174,17 +266,14 @@
     [else
      (cons (seg-posn (first s)) (segs-posn (rest s)))]))
 
-(define is (list (make-seg (make-posn 0 0) "right")
-                 (make-seg (make-posn 1 0) "right")
-                 (make-seg (make-posn 2 0) "right")
-                 (make-seg (make-posn 3 0) "right")
-                 (make-seg (make-posn 4 0) "right")
-                 (make-seg (make-posn 5 0) "right")
-                 (make-seg (make-posn 6 0) "right")
-                 (make-seg (make-posn 7 0) "right")))
+(define is
+  (make-sgame
+   (list (make-seg (make-posn 0 0) "right")
+                 (make-seg (make-posn 1 0) "right"))
+   (make-posn 6 6)))
 (define (snake-main is)
   (big-bang is
-    [on-tick move 0.25]
+    [on-tick tock 0.5]
     [on-key turn]
     [to-draw render]
     [stop-when out? render/out]))
